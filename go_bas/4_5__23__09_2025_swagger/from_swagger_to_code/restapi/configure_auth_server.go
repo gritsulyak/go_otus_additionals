6dca5ef5fd599b2d9430351dc678566da81296fc
Roman Gritsulyak <4rtgcom@gmail.com>
2025-12-25 21:47:01 +0300

    lesson results

---
+++ go_bas/4_5__23__09_2025_swagger/from_swagger_to_code/restapi/configure_auth_server.go
@@ -0,0 +1,90 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/go_otus_additionals/go_bas/4_5__23__09_2025_swagger/from_swagger_to_code/restapi/operations"
+	"github.com/go_otus_additionals/go_bas/4_5__23__09_2025_swagger/from_swagger_to_code/restapi/operations/auths"
+)
+
+//go:generate swagger generate server --target ../../from_swagger_to_code --name AuthServer --spec ../swagger.yaml --principal any
+
+func configureFlags(api *operations.AuthServerAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+	_ = api
+}
+
+func configureAPI(api *operations.AuthServerAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...any)
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.AuthsPostLoginHandler == nil {
+		api.AuthsPostLoginHandler = auths.PostLoginHandlerFunc(func(params auths.PostLoginParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation auths.PostLogin has not yet been implemented")
+		})
+	}
+	if api.AuthsPostRegisterHandler == nil {
+		api.AuthsPostRegisterHandler = auths.PostRegisterHandlerFunc(func(params auths.PostRegisterParams) middleware.Responder {
+			_ = params
+
+			return middleware.NotImplemented("operation auths.PostRegister has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+	_ = tlsConfig
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(server *http.Server, scheme, addr string) {
+	_ = server
+	_ = scheme
+	_ = addr
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
